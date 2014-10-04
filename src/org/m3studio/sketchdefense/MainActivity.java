136503639398b116d63dc91ba940aff6d09848b0
pash1s <pash1s@yandex.ru>
2014-10-04 20:53:03 +0400

    fullscreen, фон, добавлены картинки фона и кнопки

--- src/org/m3studio/sketchdefense/MainActivity.java
+++ src/org/m3studio/sketchdefense/MainActivity.java
@@ -1,22 +1,22 @@
 package org.m3studio.sketchdefense;
-
+/*
 import org.m3studio.gameengine.core.Background;
 import org.m3studio.gameengine.core.Engine;
 import org.m3studio.gameengine.core.ResourceFactory;
 import org.m3studio.gameengine.core.Sprite;
 import org.m3studio.gameengine.core.Vector;
 import org.m3studio.gameengine.core.VisibleSpriteGameObject;
-
+*/
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Toast;
+//import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Engine engine = new Engine(this);
+/*		Engine engine = new Engine(this);
 		
 		Sprite hero = ResourceFactory.getInstance().makeSpriteFromResourceStrip(R.drawable.strip3, 2, 10, 10, new Vector(20, 20));
 		Background background = ResourceFactory.getInstance().makeBackgroundFromResource(R.drawable.back, new Vector(0, 0));
@@ -32,10 +32,10 @@ public class MainActivity extends Activity {
 			engine.addVisibleGameObject(obj);
 			obj.addAnimationToAnimationPipeline();
 		}
+*/		
+		setContentView(R.layout.activity_start);
 		
-		setContentView(engine.getView());
-		
-		Toast.makeText(this, "Tap on the sprites in order to have some fun =)", Toast.LENGTH_LONG).show();
+//		Toast.makeText(this, "Tap on the sprites in order to have some fun =)", Toast.LENGTH_LONG).show();
 	}
 
 }
